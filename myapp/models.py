ba4fb44ca7f7dd8b2277aaf3cc77b4ccda0481d6
papanda <ru3aah@gmail.com>
2025-05-18 11:20:41 +0300

    end of class

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     description = models.TextField()
 
     def __str__(self):
